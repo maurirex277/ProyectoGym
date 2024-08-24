d244bad1a7aead20b6fe4e5a3d77f5a54c370410
augusto <dossantosaugusto36@gmail.com>
2024-08-24 17:35:59 -0300

    subida de proyecto

---
+++ plans.php
@@ -0,0 +1,164 @@
+<?php include('db_connect.php');?>
+
+<div class="container-fluid">
+	
+	<div class="col-lg-12">
+		<div class="row">
+			<!-- FORM Panel  -->
+			<div class="col-md-4">
+			<form action="" id="manage-plan">
+				<div class="card">
+					<div class="card-header">
+						    Crear Plan
+				  	</div>
+					<div class="card-body">
+							<input type="hidden" name="id">
+							<div class="form-group">
+								<label class="control-label">Plan (meses)</label>
+								<input type="number" class="form-control text-right" min="1" name="plan" >
+							</div>
+							<div class="form-group">
+								<label class="control-label">Precio</label>
+								<input type="number" class="form-control text-right" step="any" name="amount">
+							</div>
+							
+					</div>
+							
+					<div class="card-footer">
+						<div class="row">
+							<div class="col-md-12">
+								<button class="btn btn-sm btn-primary col-sm-3 offset-md-3"> Guardar</button>
+								<button class="btn btn-sm btn-default col-sm-3" type="button" onclick="_reset()"> Cancelar</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</form>
+			</div>
+			<!-- FORM Panel -->
+
+			<!-- Table Panel -->
+			<div class="col-md-8">
+				<div class="card">
+					<div class="card-header">
+						<b>Lista de planes</b>
+					</div>
+					<div class="card-body">
+						<table class="table table-bordered table-hover">
+							<colgroup>
+								<col width="5%">
+								<col width="55%">
+								<col width="20%">
+								<col width="20%">
+							</colgroup>
+							<thead>
+								<tr>
+									<th class="text-center">#</th>
+									<th class="text-center">Plan</th>
+									<th class="text-center">Precio</th>
+									<th class="text-center">Acción</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php 
+								$i = 1;
+								$plan = $conn->query("SELECT * FROM plans order by id asc");
+								while($row=$plan->fetch_assoc()):
+								?>
+								<tr>
+									<td class="text-center"><?php echo $i++ ?></td>
+									<td class="">
+										<p><b><?php echo $row['plan'] ?></b> Mes/es</p>
+									</td>
+									<td class="text-right">
+										<b><?php echo number_format($row['amount'],2) ?></b>
+									</td>
+									<td class="text-center">
+
+										<button class="btn btn-sm btn-primary edit_plan" type="button" data-id="<?php echo $row['id'] ?>" data-plan="<?php echo $row['plan'] ?>">Editar</button>
+
+										<button class="btn btn-sm btn-danger delete_plan" type="button" data-id="<?php echo $row['id'] ?>">Eliminar</button>
+									</td>
+								</tr>
+								<?php endwhile; ?>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+			<!-- Table Panel -->
+		</div>
+	</div>	
+
+</div>
+<style>
+	
+	td{
+		vertical-align: middle !important;
+	}
+</style>
+<script>
+	function _reset(){
+		$('#manage-plan').get(0).reset()
+		$('#manage-plan input,#manage-plan textarea').val('')
+	}
+	$('#manage-plan').submit(function(e){
+		e.preventDefault()
+		start_load()
+		$.ajax({
+			url:'ajax.php?action=save_plan',
+			data: new FormData($(this)[0]),
+		    cache: false,
+		    contentType: false,
+		    processData: false,
+		    method: 'POST',
+		    type: 'POST',
+			success:function(resp){
+				if(resp==1){
+					alert_toast("Data successfully added",'success')
+					setTimeout(function(){
+						location.reload()
+					},1500)
+
+				}
+				else if(resp==2){
+					alert_toast("Data successfully updated",'success')
+					setTimeout(function(){
+						location.reload()
+					},1500)
+
+				}
+			}
+		})
+	})
+	$('.edit_plan').click(function(){
+		start_load()
+		var cat = $('#manage-plan')
+		cat.get(0).reset()
+		cat.find("[name='id']").val($(this).attr('data-id'))
+		cat.find("[name='plan']").val($(this).attr('data-plan'))
+		cat.find("[name='amount']").val($(this).attr('data-amount'))
+		end_load()
+	})
+	$('.delete_plan').click(function(){
+		_conf("Are you sure to delete this plan?","delete_plan",[$(this).attr('data-id')])
+	})
+	function delete_plan($id){
+		start_load()
+		$.ajax({
+			url:'ajax.php?action=delete_plan',
+			method:'POST',
+			data:{id:$id},
+			success:function(resp){
+				if(resp==1){
+					alert_toast("Data successfully deleted",'success')
+					setTimeout(function(){
+						location.reload()
+					},1500)
+
+				}
+			}
+		})
+	}
+	$('table').dataTable()
+</script>
